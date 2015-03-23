f045f451032e56b00f6a1df3caf1f86a2ddad77e
eichi.mita <eichi.mita@rakus.co.jp>
2015-03-23 14:28:46 +0900

    add show line separator

--- data/js/cs-content-menu.js
+++ data/js/cs-content-menu.js
@@ -3,6 +3,8 @@ self.on("click", function (node, data) {
         url = node.getAttribute("href"),
         encoding,
         bytes,
+        codeString,
+        newline,
         md5sum,
         content;
 
@@ -11,9 +13,18 @@ self.on("click", function (node, data) {
     xhr.onload = function() {
         bytes = new Uint8Array(this.response);
         encoding = Encoding.detect(bytes);
-        md5sum = MD5_hexhash(Encoding.codeToString(bytes));
+        codeString = Encoding.codeToString(bytes);
+        md5sum = MD5_hexhash(codeString);
+        md5sumLf = MD5_hexhash(codeString.replace(/\r\n?/g, "\n"));
+        if (codeString.indexOf("\r\n", 0) > 0) {
+          newline = "CRLF";
+        } else if (codeString.indexOf("\n", 0) > 0) {
+          newline = "LF";
+        } else {
+          newline = "CR";
+        }
         content = Encoding.codeToString(Encoding.convert(bytes, "UNICODE", encoding));
-        self.postMessage({url: url, encoding: encoding, md5sum: md5sum, content: escapeHTML(content)});
+        self.postMessage({url: url, encoding: encoding, md5sum: md5sum, md5sumLf: md5sumLf, newline: newline, content: escapeHTML(content)});
     };
     xhr.send();
 });
--- data/js/cs-panel.js
+++ data/js/cs-panel.js
@@ -1,6 +1,8 @@
 self.port.on("show", function(param) {
   $("#md5sum").html(param.md5sum);
+  $("#md5sumLf").html(param.md5sumLf);
   $("#encoding").html(param.encoding);
+  $("#newline").html(param.newline);
   $("#content").html(param.content);
   $('pre code').each(function(i, block) {
     return hljs.highlightBlock(block);
